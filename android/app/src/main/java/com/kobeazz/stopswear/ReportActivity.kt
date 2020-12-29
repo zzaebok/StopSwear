a16b5c68501cac5307461374310269f62ba790ae
zzaebok <jaebok9541@naver.com>
2020-12-29 21:44:39 +0900

    resize graph and set checkable false report item

--- android/app/src/main/java/com/kobeazz/stopswear/ReportActivity.kt
+++ android/app/src/main/java/com/kobeazz/stopswear/ReportActivity.kt
@@ -1,6 +1,7 @@
 package com.kobeazz.stopswear
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.github.mikephil.charting.components.XAxis
@@ -9,6 +10,7 @@ import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
+import java.lang.Math.round
 
 // ref: https://chjune0205.tistory.com/81
 
@@ -37,10 +39,13 @@ class ReportActivity : AppCompatActivity() {
         val daysString = dataManager.getValue("days", "String").toString()
         val days = daysString.split("/").reversed()
         val onlyDates = days.map {it.split("-").slice(1..2).joinToString("-")}
-
+        var averageSwear = 0f
         days.forEachIndexed { i, day ->
-            entries.add(BarEntry(i.toFloat(), (dataManager.getValue(day, "Int") as Int).toFloat()))
+            val swearTime = dataManager.getValue(day, "Int") as Int
+            entries.add(BarEntry(i.toFloat(), (swearTime).toFloat()))
+            averageSwear += swearTime
         }
+        averageSwear /= days.size
 
         val chart = findViewById<com.github.mikephil.charting.charts.BarChart>(R.id.chart)
         chart.run {
@@ -52,9 +57,9 @@ class ReportActivity : AppCompatActivity() {
             setDrawGridBackground(false)
 
             axisLeft.run {
-                axisMaximum = 100F
-                axisMinimum = 0F
-                granularity = 25F
+                axisMaximum = 100f
+                axisMinimum = 0f
+                granularity = 25f
 
                 setDrawLabels(true)
                 setDrawGridLines(true)
@@ -62,17 +67,17 @@ class ReportActivity : AppCompatActivity() {
                 axisLineColor = ContextCompat.getColor(context, R.color.black)
                 gridColor = ContextCompat.getColor(context, R.color.material_on_primary_disabled)
                 textColor = ContextCompat.getColor(context, R.color.black)
-                textSize = 14F
+                textSize = 10f
             }
 
             xAxis.run {
                 position = XAxis.XAxisPosition.BOTTOM
-                granularity = 1F
+                granularity = 1f
                 setDrawAxisLine(true)
                 setDrawGridLines(false)
                 textColor = ContextCompat.getColor(context, R.color.black)
                 valueFormatter = IndexAxisValueFormatter(onlyDates)
-                textSize = 14F
+                textSize = 10f
             }
 
             axisRight.isEnabled = false
@@ -87,12 +92,14 @@ class ReportActivity : AppCompatActivity() {
         val datasetList = ArrayList<IBarDataSet>()
         datasetList.add(dataset)
         val barData= BarData(datasetList)
-        barData.barWidth = 0.3F
+        barData.barWidth = 0.3f
         chart.run {
             this.data = barData
             setFitBars(true)
             invalidate()
         }
+
+        findViewById<TextView>(R.id.averageSwear).text = "일 평균 ${round(averageSwear * 10)/10.0}회"
 
     }
 }
