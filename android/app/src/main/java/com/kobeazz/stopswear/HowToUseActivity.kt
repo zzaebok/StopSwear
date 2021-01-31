4231d274f1e8db63c3bcf882b029943a0b88f7dc
zzaebok <jaebok9541@naver.com>
2021-02-01 00:44:29 +0900

    Finished first version of front design

--- android/app/src/main/java/com/kobeazz/stopswear/HowToUseActivity.kt
+++ android/app/src/main/java/com/kobeazz/stopswear/HowToUseActivity.kt
@@ -1,8 +1,8 @@
 package com.kobeazz.stopswear
 
+import android.os.Build
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.TextView
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.tbuonomo.viewpagerdotsindicator.SpringDotsIndicator
@@ -15,10 +15,11 @@ class HowToUseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_how_to_use)
 
-        howToUseDataList.add(HowToUseData(R.drawable.ic_baseline_menu_24, "버튼을 눌러 나쁜말 탐지기를 작동합니다."))
-        howToUseDataList.add(HowToUseData(R.drawable.ic_baseline_menu_24, "접근성 권한을 설정해주어야 합니다."))
-        howToUseDataList.add(HowToUseData(R.drawable.ic_baseline_menu_24, "접근성 권한 사용에 동의해주세요.\n정보는 절대 서버로 보내지지 않습니다."))
-        howToUseDataList.add(HowToUseData(R.drawable.ic_baseline_menu_24, "메세지를 입력할 때 나쁜말을 탐지하면 경고가 발생합니다."))
+        howToUseDataList.add(HowToUseData(R.drawable.usage1, "버튼을 눌러 나쁜말 탐지기를 작동합니다."))
+        howToUseDataList.add(HowToUseData(R.drawable.usage2, "접근성 권한을 설정해주어야 합니다.\n나쁜말 탐지기를 클릭해주세요."))
+        howToUseDataList.add(HowToUseData(R.drawable.usage3, "접근성 권한 사용에 동의해주세요.\n정보는 절대 서버로 보내지지 않습니다."))
+        howToUseDataList.add(HowToUseData(R.drawable.usage4, "나쁜말 탐지기가 성공적으로 켜졌습니다."))
+        howToUseDataList.add(HowToUseData(R.drawable.usage5, "메세지를 입력할 때 나쁜말을 탐지하면 경고가 발생합니다."))
 
         howToUseViewHolderAdapater = HowToUseViewHolderAdapter(howToUseDataList)
 
@@ -31,8 +32,11 @@ class HowToUseActivity : AppCompatActivity() {
 
         findViewById<SpringDotsIndicator>(R.id.spring_dots_indicator).setViewPager2(pager)
 
-        findViewById<TextView>(R.id.toolbarTitle).setText("사용법")
-        findViewById<ImageButton>(R.id.toolbarBackbutton).setOnClickListener { this.onBackPressed() }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
 
     }
 }
